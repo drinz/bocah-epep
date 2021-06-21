e4eea39098916ecfd43cafbb595bb80d75ed3f11
drinz <86057200+drinz@users.noreply.github.com>
2021-06-21 17:55:47 +0700

    Create ff.py

---
+++ ff.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+
+print("--> tools hack by iki <--")
+print("#-----------#")
+print("#---hello---#")
+print("#___________#")
+ip = str(input(" username ff:"))
+port = str(input(" id:"))
+
+
+print("menghack akun ff yang bernama %s dengan id %s"%(ip,port))
+hello = str(input("apakah anda yakin untuk melihat password akun?"))
+print("password akun %s adalah prank aowkwkkw"%(ip))
+
+
+    
